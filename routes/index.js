ffb92c97e95f3ec093217d099984a22742745327
Tao <d1257272@o365.fcu.edu.tw>
2025-05-28 05:27:47 +0800

    A

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+// 刪除油價資料
+router.delete('/api/oil/:id', function(req, res) {
+  const id = req.params.id;
+  db.run('DELETE FROM oil_price WHERE id = ?', [id], function(err) {
+    if (err) {
+      res.status(500).send('刪除失敗: ' + err.message);
+    } else if (this.changes === 0) {
+      res.status(404).send('找不到該筆資料');
+    } else {
+      res.send('刪除成功');
+    }
+  });
 });
 
 module.exports = router;
